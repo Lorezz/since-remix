0872bd3f68edef5cbc5e0a05d2ce34e6378cda80
Lorezz <point.point@gmail.com>
2022-01-30 19:26:08 +0100

    added nick to post

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -2,20 +2,33 @@ import { Link, redirect, useLoaderData } from 'remix';
 import { db } from '~/lib/db.server';
 import { getUser } from '~/lib/session.server';
 
-import type { User, Post } from '@prisma/client';
+import type { User, Post, Prisma } from '@prisma/client';
 import type { ActionFunction, MetaFunction, LoaderFunction } from 'remix';
 
-type LoaderData = { user: User | null; post: Post };
+type PostWithUser = Prisma.PostGetPayload<{
+  include: {
+    user: {
+      select: {
+        nick: true;
+      };
+    };
+  };
+}>;
+
+type LoaderData = { user: User | null; post: PostWithUser };
 export const loader: LoaderFunction = async ({ request, params }) => {
   const user = await getUser(request);
 
   const post = await db.post.findUnique({
     where: { id: params.postId },
+    include: {
+      user: { select: { nick: true } },
+    },
   });
 
   if (!post) throw new Error('Post not found');
 
-  const data: LoaderData = { post, user };
+  const data: LoaderData = { user, post };
   return data;
 };
 
@@ -61,21 +74,30 @@ function Post() {
   return (
     <div className="container">
       <nav className="page-header">
-        <Link to="/posts" className="btn btn-reverse">
+        <Link
+          to="/posts"
+          className="items-center px-4 py-2 border border-transparent text-xs font-medium rounded-md text-white bg-gray-900 shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
           Back
         </Link>
       </nav>
       <div className="prose p-5">
-        <h1 className="uppercase">{post.title}</h1>
-        <div className="text-gray-800 px-2">{post.body}</div>
-        <div className="p-4">
-          {user && user.id === post.userId && (
-            <form method="POST">
-              <input type="hidden" name="_method" value="delete" />
-              <button className="btn btn-delete">Delete</button>
-            </form>
-          )}
+        <h2 className="uppercase">{post.title}</h2>
+        <div className="h6">Author: {post.user.nick}</div>
+        <div className="h6">
+          Publish on: {new Date(post.createdAt).toLocaleString()}
         </div>
+        <div className="text-gray-800 py-10 px-2">{post.body}</div>
+      </div>
+      <div>
+        {user && user.id === post.userId && (
+          <form method="POST">
+            <input type="hidden" name="_method" value="delete" />
+            <button className="items-center px-4 py-2 border border-transparent text-xs font-medium rounded-md text-white bg-red-600 shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+              Delete
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,6 +1,6 @@
-import { Link, useLoaderData } from 'remix';
+import { json, Link, useLoaderData } from 'remix';
 import { db } from '~/lib/db.server';
-import type { Post } from '@prisma/client';
+import type { Prisma } from '@prisma/client';
 import { getUser } from '~/lib/session.server';
 import type { LoaderFunction } from 'remix';
 
@@ -9,7 +9,12 @@ export const loader: LoaderFunction = async ({ request }) => {
   const data = {
     posts: await db.post.findMany({
       take: 50,
-      select: { id: true, title: true, createdAt: true },
+      select: {
+        id: true,
+        title: true,
+        user: { select: { nick: true } },
+        createdAt: true,
+      },
       orderBy: { createdAt: 'desc' },
     }),
     user,
@@ -17,7 +22,15 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   return data;
 };
-
+type PostWithUser = Prisma.PostGetPayload<{
+  include: {
+    user: {
+      select: {
+        nick: true;
+      };
+    };
+  };
+}>;
 function PostItems() {
   const { posts, user } = useLoaderData();
 
@@ -40,7 +53,7 @@ function PostItems() {
           role="list"
           className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
         >
-          {posts.map((post: Post) => (
+          {posts.map((post: PostWithUser) => (
             <li key={post.id} className="relative">
               <Link to={post.id}>
                 <>
@@ -53,6 +66,9 @@ function PostItems() {
                   </div>
                   <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
                     {post.title}
+                  </p>
+                  <p className="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">
+                    {post.user.nick}
                   </p>
                   <p className="block text-sm font-medium text-gray-500 pointer-events-none">
                     {new Date(post.createdAt).toLocaleString()}
